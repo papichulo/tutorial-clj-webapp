736d513b0e62743f1b58bafcf439a00779632455
David Simonsbacka <david.vikstrom@gmail.com>
2017-11-02 07:44:54 +0100

    Completed Task 03

--- todo-app/src/todo_app/handler.clj
+++ todo-app/src/todo_app/handler.clj
@@ -1,9 +1,15 @@
 (ns todo-app.handler
-  (:require [compojure.core :refer [defroutes GET POST DELETE]]
+  (:require [compojure.coercions :refer [as-int]]
+            [compojure.core :refer [defroutes GET POST DELETE]]
             [compojure.route :as route]
             [hiccup.page :refer [doctype include-css]]
             [hiccup2.core :refer [html]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [hiccup.form :as f]
+            [hiccup.element :refer [link-to]]
+            [todo-app.domain :refer [all-todos todo-by-id add-todo! remove-todo!]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
+            [ring.util.response :refer [redirect]]))
 
 (defn page [title & content]
   (str 
@@ -11,13 +17,44 @@
       (doctype :html5)
       [:html
         [:head (include-css "splendor.css") [:title title]]
-        [:body content]])))
+        [:body [:h1 title] content]])))
+
+(defn anti-forgery-field []
+  (f/hidden-field "__anti-forgery-token" *anti-forgery-token*))
 
 (defn index []
-  (page "TODO App" [:h1 "TODO App"]))
+  (page "TODO App"
+    [:ul 
+      (for [todo (all-todos)]
+        [:li (link-to (str "/" (:id todo)) (:text todo))])]
+    (f/form-to [:post "/"]
+      (anti-forgery-field)
+      (f/text-field "todo")
+      (f/submit-button "Add"))))
+
+(defn add-todo [todo]
+  (when (not (clojure.string/blank? todo))
+    (add-todo! todo))
+    (redirect "/"))
+
+(defn show-todo [id]
+  (let [todo (todo-by-id id)]
+    (when todo
+      (page (str "TODO " id)
+        [:h2 (:text todo)]
+        (f/form-to [:delete (str "/" id)]
+            (anti-forgery-field)
+            (f/submit-button "Delete"))))))
+
+(defn delete-todo [id]
+  (remove-todo! id)
+  (redirect "/"))
 
 (defroutes app-routes
   (GET "/" [] (index))
+  (POST "/" [todo] (add-todo todo))
+  (GET "/:id" [id :<< as-int] (show-todo id))
+  (DELETE "/:id" [id :<< as-int] (delete-todo id))
   (route/not-found "Not Found"))
 
 (def app
---
+++ todo-app/test/todo_app/03_tests.clj
@@ -0,0 +1,58 @@
+(ns todo-app.03-tests
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [hiccup.core :refer [html]]
+            [hiccup.element :refer [link-to]]
+            [ring.mock.request :as mock]
+            [todo-app.domain :refer [all-todos todo-by-id add-todo!]]
+            [todo-app.handler :refer [app app-routes]]
+            [hiccup.form :as f]))
+
+(deftest test-index
+  (testing "index"
+    (let [todos (all-todos)
+          response (app (mock/request :get "/"))
+          expected-html (html [:ul (for [todo todos] [:li (link-to (str "/" (:id todo)) (:text todo))])])]
+      (is (= (:status response) 200))
+      (is (str/includes? (:body response) "<h1>TODO App</h1>"))
+      (is (str/includes? (:body response) expected-html)))))
+
+(deftest test-add-route 
+  (testing "add route"
+    (let [oldcount (count (all-todos))
+          response (app-routes (-> (mock/request :post "/")
+                                   (assoc :params {:todo "Hi!"})))]
+      (is (= (:status response) 302))
+      (is (= (:headers response) {"Location" "/"}))
+      (is (= (count (all-todos)) (inc oldcount)))))
+  
+  (testing "Do not add if todo empty"
+    (let [oldtodos (all-todos)
+          response (app-routes (-> (mock/request :post "/")
+                                   (assoc :params {:todo ""})))]
+      (is (= (all-todos) oldtodos))))
+  
+  (testing "Escape todo if evil!"
+    (let [_ (app-routes (-> (mock/request :post "/")
+                            (assoc :params {:todo "<script>alert('hi!')</script>"})))
+          response (app-routes (app-routes (mock/request :get "/")))
+          responses (map #(app-routes (mock/request :get (str "/" (:id %)))) (all-todos))]
+      (is (not (str/includes? (:body response) "<script>")))
+      (is (not-any? #(str/includes? % "<script>") (map :body responses))))))
+
+(deftest test-show
+  (testing "show route"
+    (add-todo! "Make sure not empty!")
+    (let [first    (first (all-todos))
+          response (app (mock/request :get (str "/" (:id first))))]
+       (is (:status response) 200)
+       (is (str/includes? (:body response) (:text first))))))
+  
+(deftest test-delete
+  (testing "delete route"
+    (let [oldtodos (all-todos)
+          todo     (add-todo! "To Delete")
+          response (app-routes (mock/request :delete (str "/" (:id todo))))]
+      (is (= (all-todos) oldtodos))
+      (is (:status response) 302)
+      (is (= (:headers response) {"Location" "/"})))))
